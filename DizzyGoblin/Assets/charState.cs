8432b62234bd3c23c0cdbab595647c0c1e5c89d4
eTveit <espen.tveit@hotmail.no>
2017-05-09 12:02:24 +0200

    Get back the spinning hips

--- DizzyGoblin/Assets/charState.cs
+++ DizzyGoblin/Assets/charState.cs
@@ -8,19 +8,25 @@ public class charState : MonoBehaviour {
 
     public Transform terrain = null;
     public float heightOffset = 0;
-	public float speed = 1.5f;
-    
-	// Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    
+
+    public float speed = 2.5f;
+
+    public float rotationSpeed = 0;
+    public float rotationBoost = 0;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
         float x = transform.position.x;
         float z = transform.position.z;
-		
+        
+
+
         //TODO: refactor this into a control FSM 
         //(see steering from last semester)
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -47,5 +53,21 @@ public class charState : MonoBehaviour {
         //and set new position
         transform.position = Vector3.Lerp(transform.position, goalpos, Time.deltaTime * 10);
 
+        if(rotationSpeed != 0) {
+            Rotate();
+        }
+    }
+
+    void Rotate() {
+        if(Mathf.Cos((Time.time * 4) + 3.141593f) > 0.9f) {
+            rotationBoost = 50;
+        }
+        else {
+            rotationBoost = 0;
+        }
+
+        // this is not physics, it should be rebuilt
+        Quaternion rotation = Quaternion.AngleAxis(-(rotationSpeed + rotationBoost) * Time.deltaTime, transform.up);
+        transform.rotation *= rotation;
     }
 }
--- DizzyGoblin/Assets/targetMove.cs
+++ DizzyGoblin/Assets/targetMove.cs
@@ -20,6 +20,8 @@ public class targetMove : MonoBehaviour {
 
 	public Transform AvatarObj;
 	private charState AvatarState;
+    //for circular movement
+    public float circularHeight = -1;
 
 	// Use this for initialization
 	void Start () {
@@ -31,11 +33,22 @@ public class targetMove : MonoBehaviour {
 	void Update ()
     {
 		speed = AvatarState.speed;
+    
+        float ypos = -666;
+
+
         //to keep our targets in line with the hips, we simply want to
         //oscillate on z axis in the LOCAL space
 
         Vector3 lpos = transform.localPosition;
         lpos.Set(lpos.x, lpos.y, Mathf.Sin((Time.time * speed) + phase) * range);
+
+        if(circularHeight > 0) {
+            ypos = Mathf.Cos((Time.time * speed) + phase + 3.141593f) * circularHeight;
+            ypos = -ypos;
+            lpos.y = ypos;
+        }
+
 
         //set the local
         transform.localPosition = lpos;
@@ -44,6 +57,10 @@ public class targetMove : MonoBehaviour {
         Vector3 pos = transform.position;
         float y = mesh.getHeightAt(pos);
         pos.y = y + 0.2f;
+
+        if(ypos > 0) {
+            pos.y += ypos;
+        }
 
         //set the final position
         transform.position = pos;

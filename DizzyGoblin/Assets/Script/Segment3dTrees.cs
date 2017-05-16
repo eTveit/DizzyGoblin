797751e81e14e687cfce693ab682ebb909191276
JulianHolm <julrholm@gmail.com>
2017-05-16 16:15:15 +0200

    JH_TReeAnimations_1

--- DizzyGoblin/Assets/Script/Segment3dTrees.cs
+++ DizzyGoblin/Assets/Script/Segment3dTrees.cs
@@ -15,11 +15,24 @@ public class Segment3dTrees : MonoBehaviour
     
     private Vector3 goalTarget;
 
+    //the ik system will tell it's children who owns it
+    public IKSystem3dTrees IKsys = null;
+    private Quaternion initialRotation;
+
     void Start()
     {
        
     }
+    private void Awake()
+    {
+        //calculate the length from me to my child, based on my initial
+        //pivot positions that we manually manipulated in the editor
+        if (child)
+        {
+            length = Vector3.Distance(transform.position, child.transform.position);
+        }
 
+    }
     public void updateSegmentAndChildren()
     {
 		
@@ -58,9 +71,54 @@ public class Segment3dTrees : MonoBehaviour
     
     public void pointAt(Vector3 target)
     {
+
+        //get current rot if we interp    
+        Quaternion prot = transform.localRotation;
+
+        //get unclamped desire rotation as a quat, an euler, and a direction;
         transform.LookAt(target);
-		
-	}
+
+        Vector3 sysright = IKsys.transform.right;
+        Vector3 segright = transform.right;
+
+        Vector3 sysup = IKsys.transform.forward;
+        Vector3 segup = transform.forward;
+
+
+        //get an alignment to the parent transform (the ik system itself)
+        float aZ = Vector3.Angle(segright, sysright);
+       
+        //invert where needed based on the direction of the angle
+        int iZ = AngleDirInt(segright, sysright, sysup);
+        
+        aZ *= iZ;
+        
+        //get my new rotation in local
+        Quaternion  rot = transform.localRotation;
+        Vector3 euler = rot.eulerAngles;
+
+
+        float x = euler.x;
+        float z = 0; // euler.z;
+        float y = euler.y;
+
+      
+        euler.Set( x ,
+                   y ,
+                   z * aZ );
+
+        /*
+        if (interpolate)
+        {
+            goalEuler = euler;          //set the goal rotation
+            euler = prot.eulerAngles;    //reset the current rotation
+            euler = Vector3.Slerp(euler, goalEuler, Time.deltaTime * interpRate);
+        }
+        */
+
+        transform.localRotation = Quaternion.Euler(euler.x, euler.y, euler.z);
+
+    }
 
     public void drag(Vector3 target)
     {
@@ -78,5 +136,25 @@ public class Segment3dTrees : MonoBehaviour
     {
         drag(target);
         updateSegment();
+    }
+
+    //cross/dot product method returns v2 is "left" or "right" of v1 
+    int AngleDirInt(Vector3 v1, Vector3 v2, Vector3 up)
+    {
+        Vector3 perp = Vector3.Cross(v1, v2);
+        float dir = Vector3.Dot(perp, up);
+
+        if (dir > 0f)
+        {
+            return 1;
+        }
+        else if (dir < 0f)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
     }
 }
---
+++ DizzyGoblin/Assets/Script/TargetScripts/treeAnimate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class treeAnimate : IKAnimationTarget
+{
+
+    //DONT FORGET TO RE-NAME IT, YOUR INITIALS, AND SOME LOGICAL NAME
+    //the FSM uses names of our creation to select animations to play
+    /*
+         "JK_walk";
+    */
+    public override string getAnimName()
+    {
+        return animationName;
+    }
+    
+    
+    //phase determines the relationship between multiple move points
+    //as a function of PI, as Sin is the oscillating function
+    public float phase = 0;
+    
+    //how fast the target point moves
+    public float speed = 1;
+
+    //the range of motion of the move point
+    public float range = 1; 
+
+        
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+		//we need to smoothly transition to the new start point before running the animation
+		if (interpolateToStartPosition(Time.deltaTime, speed) == false)
+			return;
+
+	
+        //to keep our targets in line with the hips, we simply want to
+        //oscillate on z axis in the LOCAL space
+
+        Vector3 lpos = transform.localPosition;
+        lpos.Set(Mathf.Sin((Time.time * speed) + phase) * range, lpos.y, Mathf.Sin((Time.time * speed * 2) + phase) * range);
+
+
+        //set the local
+        transform.localPosition = lpos;
+        
+
+
+
+    }
+}

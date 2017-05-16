b7ccfa34d5833d9e81ae0b23e7fa1694d31d4dcf
Struddflick <ole.oleandreas@gmail.com>
2017-05-16 13:08:20 +0200

    OAS_PoolingExperiment

---
+++ DizzyGoblin/Assets/PoolSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolSystem : MonoBehaviour {
+    public GameObject SpawnPoint1;
+    public GameObject SpawnPoint2;
+    public GameObject targetTp;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            targetTp.transform.position = SpawnPoint1.transform.position;
+
+
+
+        }
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Rats" && Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            targetTp.transform.position = SpawnPoint2.transform.position;
+        }
+
+    }
+
+
+
+
+}
+
+
+

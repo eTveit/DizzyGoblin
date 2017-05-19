f50e79b53bfda580b0f5be3135d9d6d6db798bd4
Struddflick <ole.oleandreas@gmail.com>
2017-05-19 09:08:24 +0200

    Working on PoolSystem

--- DizzyGoblin/Assets/PoolSystem.cs
+++ DizzyGoblin/Assets/PoolSystem.cs
@@ -2,47 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolSystem : MonoBehaviour {
-    public GameObject SpawnPoint1;
-    public GameObject SpawnPoint2;
-    public GameObject targetTp;
+public class Poolsystem : MonoBehaviour {
+    public GameObject Rathole;
+    public GameObject Pool;
+    GameObject RatSelect;
+    public GameObject[] Rats;
+    int RatIndex;
+    GameObject tpObject;
+ 
+    
+
+    
 
 
 
-    // Use this for initialization
-    void Start()
-    {
 
 
-    }
+	// Use this for initialization
+	void Start () {
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
+       
+
+      
+        }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.G))
         {
-            targetTp.transform.position = SpawnPoint1.transform.position;
-
-
-
+            RatSelection();
         }
 
+        //if (RatHp == 0) {
+        //Rat.transform.position = pool.transform.position;
+
+        //}
     }
 
-    void OnTriggerStay(Collider other)
+    void RatSelection()
     {
-
-        if (other.gameObject.tag == "Rats" && Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            targetTp.transform.position = SpawnPoint2.transform.position;
-        }
+            Rats = GameObject.FindGameObjectsWithTag("Rats");
+            RatIndex = Random.Range(0, Rats.Length);
+            RatSelect = Rats[RatIndex];
+            tpObject = RatSelect;
+            tpObject.transform.position = Rathole.transform.position;
 
     }
 
+        
+        
+        
+       
+       
+
+
+    }
+    
+      
+    
+    
+     
 
 
 
-}
+
+
+
 
 
 

7d7c9d9f9f9783b33cbe70f5e1322eb87a5502d2
rubenjunger <ruben.junger@gmail.com>
2017-05-22 12:36:27 +0200

    SquatSquat SquatSquatSquat
    The goblin now Squats down by pressing and holding the "Q" key.

--- DizzyGoblin/Assets/Script/TargetScripts/SquatSpine.cs
+++ DizzyGoblin/Assets/Script/TargetScripts/SquatSpine.cs
@@ -16,7 +16,8 @@ public class SquatSpine : IKAnimationTarget
     {
         return animationName;
     }
-    
+
+    Vector3[] positions;
 
     //we probably always want these references
     public Transform AvatarObj;
@@ -26,48 +27,73 @@ public class SquatSpine : IKAnimationTarget
     //phase determines the relationship between multiple move points
     //as a function of PI, as Sin is the oscillating function
     public float phase = 0;
-    
+
     //how fast the target point moves
     public float speed = 1;
 
     //the range of motion of the move point
-    public float range = 1; 
+    public float range = 1;
 
     //how high the movepoint sits above terrain surface
     public float heightOffset = 0;
-        
 
-	// Use this for initialization
-	void Start () {
+    private int curPos = 0;
 
-		goblinGlobals = AvatarObj.GetComponent<GoblinGlobals> ();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    public bool squatState = false;
+
+    // Use this for initialization
+    void Start()
     {
 
-		//we need to smoothly transition to the new start point before running the animation
-		if (interpolateToStartPosition(Time.deltaTime, speed) == false)
-			return;
+        positions = new Vector3[2];
 
-		speed = goblinGlobals.speed;
-    
-        //to keep our targets in line with the hips, we simply want to
-        //oscillate on z axis in the LOCAL space
+        positions[0] = new Vector3(0f, 7.55f, 0.1300001f);
+        positions[1] = new Vector3(0, 4.66f, -1.92f);
 
-        Vector3 lpos = transform.localPosition;
-        lpos.Set(0, 4.5f, -3.159f);
-
-
-        //set the local
-        transform.localPosition = lpos;
-        
-        //get the global, keep the target on the terrain surface
-        
-        //set the final position
-       
+        goblinGlobals = AvatarObj.GetComponent<GoblinGlobals>();
 
 
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //we need to smoothly transition to the new start point before running the animation
+            if (interpolateToStartPosition(Time.deltaTime, speed) == false)
+                return;
+
+            speed = goblinGlobals.speed;
+
+            //to keep our targets in line with the hips, we simply want to
+            //oscillate on z axis in the LOCAL space
+
+            Vector3 lpos = transform.localPosition;
+
+            Vector3 goalPos = positions[curPos];
+
+            float dist = Vector3.Distance(lpos, goalPos);
+
+            //we can use an sdjust to interp FASTER the closer we are to the goal
+            float adjust = (3.0f - dist);
+
+            //if it is too small, clamp it, otherwise we wont get anywhere
+            if (adjust < 0.5f)
+                adjust = 0.5f;
+
+            lpos = Vector3.Slerp(lpos, goalPos, Time.deltaTime * adjust);
+
+            transform.localPosition = lpos;
+            if (dist < 0.1f)
+                curPos++;
+            else if (squatState && dist < 0.1f)
+                curPos++;
+
+            if (curPos >= positions.Length && squatState == true)
+                curPos = 0;
+        }
+    }
 }
+
+

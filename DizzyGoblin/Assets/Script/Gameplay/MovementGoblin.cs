9a5f7e100e6a4c35b36cf56eaaf81713f34dfde3
sykud <thieu.le@studentdomene.noroff.no>
2017-05-22 14:11:30 +0200

    Some fixes and a roll back of movement
    Didn't get to add the acceleration and decelleration to the movement
    script and rolled it back to the same script as GoblinGlobals. and som
    fixes for health and teleport script

--- DizzyGoblin/Assets/Script/Gameplay/MovementGoblin.cs
+++ DizzyGoblin/Assets/Script/Gameplay/MovementGoblin.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementGoblin : MonoBehaviour {
-    
+
 
     public Transform terrain = null;
     public float heightOffset = 0;
 
-    public float speed = 2.5f;
+    public float speed = 30.0f;
 
-    
+
     // Use this for initialization
     void Start()
     {
@@ -23,19 +23,19 @@ public class MovementGoblin : MonoBehaviour {
         float x = transform.position.x;
         float z = transform.position.z;
 
-    
+
         //TODO: refactor this into a control FSM 
         //(see steering from last semester)
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
             x = x + speed * Time.deltaTime;
 
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
             x = x - speed * Time.deltaTime;
 
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
             z = z - speed * Time.deltaTime;
 
-        if(Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
             z = z + speed * Time.deltaTime;
 
         float y = transform.position.y;
@@ -49,5 +49,91 @@ public class MovementGoblin : MonoBehaviour {
         //interp to position quickly
         //and set new position
         transform.position = Vector3.Lerp(transform.position, goalpos, Time.deltaTime * 10);
+
+
+
     }
+
+    //this searches for a Transform specifically
+    //TODO: make it search for anything, and cast it when it returns 
+    public Transform Search(Transform target, string name)
+    {
+        if (target.name == name) return target;
+
+        for (int i = 0; i < target.childCount; i++)
+        {
+            //we use "var" because the component could be anything
+            var result = Search(target.GetChild(i), name);
+
+            if (result != null) return result;
+        }
+
+        return null;
+    }
+
+
 }
+    /*[Phong]Didn't get the code to work 
+     * 
+    public Transform terrain = null;
+    public float heightOffset = 0;
+    public float maxSpeed = 50;
+    public float acceleration = 10;
+    public float deceleration = 10;
+    public float speed = 0;
+    public float xSpeed;
+    public float zSpeed;
+      
+        float dt = Time.deltaTime;
+
+        float x = transform.position.x;
+        float z = transform.position.z;
+
+
+        //TODO: refactor this into a control FSM 
+        //(see steering from last semester)
+
+        if ((Input.GetKey(KeyCode.A)) && (xSpeed < maxSpeed))
+            xSpeed = xSpeed - acceleration * dt;
+
+        else if ((Input.GetKey(KeyCode.D)) && (xSpeed > -maxSpeed))
+            xSpeed = xSpeed + acceleration * dt;
+
+        if ((Input.GetKey(KeyCode.W)) && (zSpeed < maxSpeed))
+            zSpeed = zSpeed - acceleration * dt;
+
+        else if ((Input.GetKey(KeyCode.S)) && (zSpeed > -maxSpeed))
+            zSpeed = zSpeed + acceleration * dt;
+       
+        else {
+            if (xSpeed > deceleration * dt)
+                xSpeed = xSpeed - deceleration * dt;
+
+        else if (xSpeed < -deceleration * dt)
+                xSpeed = xSpeed + deceleration * dt;
+
+            if (zSpeed > deceleration * dt)
+                zSpeed = zSpeed - deceleration * dt;
+
+            else if (zSpeed < -deceleration * dt)
+                zSpeed = zSpeed + deceleration * dt;
+
+            else
+            xSpeed = 0;
+            zSpeed = 0;
+        }
+
+        float y = transform.position.y;
+
+        // place the dude
+        //first  get the exact height
+        float goalY = terrain.GetComponent<TerrainMesh>().getHeightAt(new Vector3(x, 1, z)) + heightOffset;
+
+        Vector3 goalpos = new Vector3(x, goalY, z);
+
+        //interp to position quickly
+        //and set new position
+
+
+        transform.position = Vector3.Lerp(transform.position, goalpos, Time.deltaTime * 10);
+        */

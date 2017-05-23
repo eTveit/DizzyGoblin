21d47fcca886b8f31b4e85088d57d38522cf8773
johnklima <klima.de.sa@gmail.com>
2017-05-23 09:42:08 +0100

    added rootstate ref to ballhit

--- DizzyGoblin/Assets/Script/Gameplay/BallHit.cs
+++ DizzyGoblin/Assets/Script/Gameplay/BallHit.cs
@@ -6,6 +6,7 @@ public class BallHit : MonoBehaviour {
 
     public bool spinningLeft = false;
 
+    public RootState rootState = null;
 
     private void OnCollisionEnter(Collision collision) {
         if(collision.collider.tag == "Tree") {

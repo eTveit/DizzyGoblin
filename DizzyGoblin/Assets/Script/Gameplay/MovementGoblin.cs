ca4149d5e27ae9bb419fb9cedfa3bb8464b2c5c6
eTveit <espen.tveit@hotmail.no>
2017-06-03 00:36:26 +0200

    Implement support for controller
    For basic movement, at least.
    * Also constrained diagonal movement speed
    * Modified Input Manager slightly
    * Also did some auto formatting on MovementGoblin. That caused a stupid
    amount of changes.
    
    Small reflection:
    If the entire team had the same auto format settings, we would be able
    to auto format other members' scripts without causing lots of changes.

--- DizzyGoblin/Assets/Script/Gameplay/MovementGoblin.cs
+++ DizzyGoblin/Assets/Script/Gameplay/MovementGoblin.cs
@@ -10,33 +10,52 @@ public class MovementGoblin : MonoBehaviour {
 
     public float speed = 30.0f;
 
+    public bool newSystem = true;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
     void Update() {
 
+        
         float x = transform.position.x;
         float z = transform.position.z;
 
+        //This system should work just as well with keyboard as with controller
+        if(newSystem) {
+            float xMove = Input.GetAxis("Horizontal");
+            float zMove = Input.GetAxis("Vertical");
 
-        //TODO: refactor this into a control FSM 
-        //(see steering from last semester)
-        if (Input.GetKey(KeyCode.A))
-            x = x + speed * Time.deltaTime;
+            float moveSum = Mathf.Abs(xMove) + Mathf.Abs(zMove);
 
-        if (Input.GetKey(KeyCode.D))
-            x = x - speed * Time.deltaTime;
+            //Restrain to keep goblin from moving faster when moving diagonally
+            if(moveSum > 1) {
+                xMove /= moveSum;
+                zMove /= moveSum;
+            }
 
-        if (Input.GetKey(KeyCode.W))
-            z = z - speed * Time.deltaTime;
+            x -= xMove * speed * Time.deltaTime;
+            z -= zMove * speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.S))
-            z = z + speed * Time.deltaTime;
+        }
+        else {
+            //TODO: refactor this into a control FSM 
+            //(see steering from last semester)
+            if(Input.GetKey(KeyCode.A))
+                x = x + speed * Time.deltaTime;
+
+            if(Input.GetKey(KeyCode.D))
+                x = x - speed * Time.deltaTime;
+
+            if(Input.GetKey(KeyCode.W))
+                z = z - speed * Time.deltaTime;
+
+            if(Input.GetKey(KeyCode.S))
+                z = z + speed * Time.deltaTime;
+        }
 
         float y = transform.position.y;
 
@@ -56,16 +75,16 @@ public class MovementGoblin : MonoBehaviour {
 
     //this searches for a Transform specifically
     //TODO: make it search for anything, and cast it when it returns 
-    public Transform Search(Transform target, string name)
-    {
-        if (target.name == name) return target;
+    public Transform Search(Transform target, string name) {
+        if(target.name == name)
+            return target;
 
-        for (int i = 0; i < target.childCount; i++)
-        {
+        for(int i = 0; i < target.childCount; i++) {
             //we use "var" because the component could be anything
             var result = Search(target.GetChild(i), name);
 
-            if (result != null) return result;
+            if(result != null)
+                return result;
         }
 
         return null;
@@ -73,67 +92,67 @@ public class MovementGoblin : MonoBehaviour {
 
 
 }
-    /*[Phong]Didn't get the code to work 
-     * 
-    public Transform terrain = null;
-    public float heightOffset = 0;
-    public float maxSpeed = 50;
-    public float acceleration = 10;
-    public float deceleration = 10;
-    public float speed = 0;
-    public float xSpeed;
-    public float zSpeed;
-      
-        float dt = Time.deltaTime;
+/*[Phong]Didn't get the code to work 
+ * 
+public Transform terrain = null;
+public float heightOffset = 0;
+public float maxSpeed = 50;
+public float acceleration = 10;
+public float deceleration = 10;
+public float speed = 0;
+public float xSpeed;
+public float zSpeed;
 
-        float x = transform.position.x;
-        float z = transform.position.z;
+    float dt = Time.deltaTime;
 
-
-        //TODO: refactor this into a control FSM 
-        //(see steering from last semester)
-
-        if ((Input.GetKey(KeyCode.A)) && (xSpeed < maxSpeed))
-            xSpeed = xSpeed - acceleration * dt;
-
-        else if ((Input.GetKey(KeyCode.D)) && (xSpeed > -maxSpeed))
-            xSpeed = xSpeed + acceleration * dt;
-
-        if ((Input.GetKey(KeyCode.W)) && (zSpeed < maxSpeed))
-            zSpeed = zSpeed - acceleration * dt;
-
-        else if ((Input.GetKey(KeyCode.S)) && (zSpeed > -maxSpeed))
-            zSpeed = zSpeed + acceleration * dt;
-       
-        else {
-            if (xSpeed > deceleration * dt)
-                xSpeed = xSpeed - deceleration * dt;
-
-        else if (xSpeed < -deceleration * dt)
-                xSpeed = xSpeed + deceleration * dt;
-
-            if (zSpeed > deceleration * dt)
-                zSpeed = zSpeed - deceleration * dt;
-
-            else if (zSpeed < -deceleration * dt)
-                zSpeed = zSpeed + deceleration * dt;
-
-            else
-            xSpeed = 0;
-            zSpeed = 0;
-        }
-
-        float y = transform.position.y;
-
-        // place the dude
-        //first  get the exact height
-        float goalY = terrain.GetComponent<TerrainMesh>().getHeightAt(new Vector3(x, 1, z)) + heightOffset;
-
-        Vector3 goalpos = new Vector3(x, goalY, z);
-
-        //interp to position quickly
-        //and set new position
+    float x = transform.position.x;
+    float z = transform.position.z;
 
 
-        transform.position = Vector3.Lerp(transform.position, goalpos, Time.deltaTime * 10);
-        */
+    //TODO: refactor this into a control FSM 
+    //(see steering from last semester)
+
+    if ((Input.GetKey(KeyCode.A)) && (xSpeed < maxSpeed))
+        xSpeed = xSpeed - acceleration * dt;
+
+    else if ((Input.GetKey(KeyCode.D)) && (xSpeed > -maxSpeed))
+        xSpeed = xSpeed + acceleration * dt;
+
+    if ((Input.GetKey(KeyCode.W)) && (zSpeed < maxSpeed))
+        zSpeed = zSpeed - acceleration * dt;
+
+    else if ((Input.GetKey(KeyCode.S)) && (zSpeed > -maxSpeed))
+        zSpeed = zSpeed + acceleration * dt;
+
+    else {
+        if (xSpeed > deceleration * dt)
+            xSpeed = xSpeed - deceleration * dt;
+
+    else if (xSpeed < -deceleration * dt)
+            xSpeed = xSpeed + deceleration * dt;
+
+        if (zSpeed > deceleration * dt)
+            zSpeed = zSpeed - deceleration * dt;
+
+        else if (zSpeed < -deceleration * dt)
+            zSpeed = zSpeed + deceleration * dt;
+
+        else
+        xSpeed = 0;
+        zSpeed = 0;
+    }
+
+    float y = transform.position.y;
+
+    // place the dude
+    //first  get the exact height
+    float goalY = terrain.GetComponent<TerrainMesh>().getHeightAt(new Vector3(x, 1, z)) + heightOffset;
+
+    Vector3 goalpos = new Vector3(x, goalY, z);
+
+    //interp to position quickly
+    //and set new position
+
+
+    transform.position = Vector3.Lerp(transform.position, goalpos, Time.deltaTime * 10);
+    */

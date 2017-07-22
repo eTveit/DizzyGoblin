5e54e20dcbdeb77070954af0ba669bf5d2954df9
eTveit <espen.tveit@hotmail.no>
2017-07-23 00:18:29 +0200

    Add super simple Main Menu

---
+++ DizzyGoblin/Assets/Script/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private void OnMouseDown() {
+        if(name == "PlayText") {
+            SceneManager.LoadScene(1);
+        }
+        else if(name == "ExitText") {
+            Application.Quit();
+        }
+    }
+}
